8c67e64eeded4bd72e9c045d0e9c13ceeba18305
Roger Paris <roger.parisvicent@gmail.com>
2016-01-07 16:00:37 +0100

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+Product.delete_all
+Product.create!(title: 'CoffeScript',
+                description:
+                    '<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>',
+                image_url: 'coffeescript.jpg',
+                price: 29.95)
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+                description:
+                    '<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>',
+                image_url: 'ruby.jpg',
+                price: 49.95)
